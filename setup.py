6b29fc6c8f83d567fc38332b73b9d4dcc4f74d48
Grey Li <withlihui@gmail.com>
2018-05-26 12:01:31 +0800

    Update setup.py, use markdown for long_desc

--- setup.py
+++ setup.py
@@ -16,19 +16,24 @@ Go to `Github page
 <https://github.com/greyli/flask-ckeditor>`_ , which you can check for more
 details.
 """
+from os import path
+from codecs import open
 from setuptools import setup
 
+basedir = path.abspath(path.dirname(__file__))
+with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='Flask-CKEditor',
     version='0.3.4',
     url='http://github.com/greyli/flask-ckeditor',
-    download_url = 'https://github.com/greyli/flask-ckeditor/archive/0.3.4.tar.gz',
     license='MIT',
     author='Grey Li',
     author_email='withlihui@gmail.com',
-    description=('CKEditor integration for Flask.'),
-    long_description=__doc__,
+    description='CKEditor integration for Flask.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['flask_ckeditor'],
     zip_safe=False,
     include_package_data=True,
@@ -43,6 +48,13 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
