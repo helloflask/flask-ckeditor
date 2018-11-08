cc9460e4b3d673cad0f49c9df300609af45d0a8d
Grey Li <withlihui@gmail.com>
2018-11-08 16:41:39 +0800

    Bump version nubmer in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Flask-CKEditor',
-    version='0.4.2',
+    version='0.4.3',
     url='http://github.com/greyli/flask-ckeditor',
     license='MIT',
     author='Grey Li',
